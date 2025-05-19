f3e4d4fb9db983f921d01cfd8e4c0de7c0b6551e
Elijah Triumph <eliyang256@outlook.com>
2025-05-19 14:18:05 +0300

    change class details

--- academics/classes/models.py
+++ academics/classes/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Class(models.Model):
-    name = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 7)], unique=True)
+    number = models.DecimalField(choices=[(i, str(i)) for i in range(1, 7)], unique=True, max_digits=1, decimal_places=0)
 
     def __str__(self):
-        return f"{self.name} ({self.code})"
+        return f"S.{self.number}"
 
