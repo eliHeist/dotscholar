e7e58eb49d9f3144fd58244a5d47e318b9cd0890
Elijah Triumph <eliyang256@outlook.com>
2025-05-19 14:57:36 +0300

    school model refresh

--- schools/schools/models.py
+++ schools/schools/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
 class School(models.Model):
+
     name = models.CharField(_("Name"), max_length=50)
-    registration_number = models.CharField(_("Reg"), max_length=50, unique=True)
+    registration_number = models.CharField(_("Reg no."), max_length=50, unique=True)
     logo = models.FileField(_("Logo"), upload_to='school_logos', max_length=100)
+
+    class Meta:
+        verbose_name = _("School")
+        verbose_name_plural = _("Schools")
+
+    def __str__(self):
+        return self.name
+
+
     
