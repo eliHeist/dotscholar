1bbd5db8caf4bf9146f428081bbaf6a95b0b70a9
Elijah Triumph <eliyang256@outlook.com>
2025-05-19 13:44:38 +0300

    year model

--- schools/periods/models.py
+++ schools/periods/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from schools.schools.models import School
+
 # Create your models here.
+class Year(models.Model):
+    year = models.DecimalField(max_digits=4, decimal_places=0)
+    school = models.ForeignKey(School, on_delete=models.DO_NOTHING)
+    
+    class Meta:
+        ordering = ["-year"]
+        get_latest_by = "year"
+        order_with_respect_to = 'school'
+        verbose_name = "year"
+        verbose_name_plural = "years"
+        unique_together = ["year", "school"]
+
+    def __str__(self):
+        return self.name
