0fc7f4128759f52a1f636cf3e004570772ee7f5b
Elijah Triumph <eliyang256@outlook.com>
2025-05-21 17:06:01 +0300

    update model references and gender field on student

--- people/students/models.py
+++ people/students/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from schools.schools.models import School
 from academics.subjects.models import Paper
+from schools.schools.models import School
+from schools.streams.models import Stream
 
 # Create your models here.
+class GenderOptions(models.TextChoices):
+    MALE = 'M', 'Male'
+    FEMALE = 'F', 'Female'
+    
 class Student(models.Model):
     school = models.ForeignKey(School, verbose_name=_("School"), on_delete=models.CASCADE, related_name="students")
     date_added = models.DateField(_("Date added to system"), auto_now_add=True)
@@ -12,10 +17,11 @@ class Student(models.Model):
     first_name = models.CharField(_("First Name"), max_length=25)    
     middle_name = models.CharField(_("Middle Name"), max_length=25, null=True, blank=True)    
     last_name = models.CharField(_("Last Name"), max_length=25)
-    gender = models.CharField(_("Gender"), max_length=5)
+    gender = models.CharField(_("Gender"), max_length=1, choices=GenderOptions.choices)
     payment_code = models.CharField(_("Payment Code"), max_length=50)
     
-    current_optional_papers = models.ManyToManyField(Paper, verbose_name=_("Optional Papers"), blank=True)
+    current_optional_papers = models.ManyToManyField(Paper, verbose_name=_("Optional Papers"), blank=True, related_name="current_students")
+    current_stream = models.ForeignKey(Stream, verbose_name=_("Current Stream"), on_delete=models.PROTECT, related_name="current_students")
 
     class Meta:
         verbose_name = _("Student")
--- schools/streams/models.py
+++ schools/streams/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from academics.classes.models import Class
+from schools.schools.models import School
+
 # Create your models here.
 class Stream(models.Model):
 
     name = models.CharField(_("Name"), max_length=50)
-    school = models.ForeignKey("schools.School", verbose_name=_("School"), on_delete=models.CASCADE)
-    current_class = models.ForeignKey("classes.Class", verbose_name=_("Class"), on_delete=models.CASCADE)
+    school = models.ForeignKey(School, verbose_name=_("School"), on_delete=models.CASCADE)
+    current_class = models.ForeignKey(Class, verbose_name=_("Class"), on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _("Stream")
