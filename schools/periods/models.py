11affefc5d2050fc1b8d0e9db04f821bc02d1ab4
Elijah Triumph <eliyang256@outlook.com>
2025-05-21 17:55:48 +0300

    year model

--- schools/periods/models.py
+++ schools/periods/models.py
@@ -1,11 +1,29 @@
+import datetime
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from schools.schools.models import School
 
 # Create your models here.
+current_year = datetime.date.today().year
+min_year = 2010
+
+today = datetime.date.today()
+sept_1 = datetime.date(today.year, 9, 1)
+
+years_add = 2 if today >= sept_1 else 1
+
+YEAR_CHOICES = [(r, str(r)) for r in range(min_year, current_year + years_add)][::-1]
+
 class Year(models.Model):
-    year = models.DecimalField(max_digits=4, decimal_places=0)
+    year = models.IntegerField(
+        choices=YEAR_CHOICES,
+        default=current_year,
+        verbose_name=_("Year"),
+        validators=[MinValueValidator(min_year), MaxValueValidator(current_year)]
+    )
     school = models.ForeignKey(School, on_delete=models.DO_NOTHING)
     
     class Meta:
