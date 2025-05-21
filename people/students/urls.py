ae655059676d48982a4cbbb2d4960182acc45921
Elijah Triumph <eliyang256@outlook.com>
2025-05-21 16:13:06 +0300

    add url

--- people/students/urls.py
+++ people/students/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from . import views
+
 app_name = "students"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.StudentListView.as_view(), name="student-list"),
+]
