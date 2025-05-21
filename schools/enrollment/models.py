cfb411b0ba7e8c39bd6ebdf9155358146205127a
Elijah Triumph <eliyang256@outlook.com>
2025-05-21 17:28:37 +0300

    add emrollment model

--- schools/enrollment/models.py
+++ schools/enrollment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from people.students.models import Student
+from schools.streams.models import Stream
+from schools.terms.models import Term
 
 # Create your models here.
+class Enrollment(models.Model):
+    term = models.ForeignKey(Term, verbose_name=_("Term"), on_delete=models.CASCADE)
+    stream = models.ForeignKey(Stream, verbose_name=_("Stream"), on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, verbose_name=_("Student"), on_delete=models.CASCADE)
+    date_enrolled = models.DateField(_("Date Enrolled"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Enrollment")
+        verbose_name_plural = _("Enrollments")
+
+    def __str__(self):
+        return self.student.get_full_name()
