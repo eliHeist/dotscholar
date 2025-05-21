eb60293b7dd7b2ab2146566332cf8afa930da8fd
Elijah Triumph <eliyang256@outlook.com>
2025-05-21 15:58:17 +0300

    starter of students list view

--- people/students/views.py
+++ people/students/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Student
+
 # Create your views here.
+class StudentListView:
+    def get(self, request):
+        # Logic to retrieve and display a list of students
+        students = Student.objects.all()
+        context = {
+            'students': students,
+        }
+        return render(request, 'students/student-list.html', context)
+
+
