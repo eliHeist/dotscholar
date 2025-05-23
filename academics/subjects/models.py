5ba336a54771874e7242a20fd547be2117d0f17c
Elijah Triumph <eliyang256@outlook.com>
2025-05-23 11:31:28 +0300

    add category, level, code to Subjects

--- academics/subjects/models.py
+++ academics/subjects/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from academics.classes.models import Class, LevelChoices
+
 # Create your models here.
 class Subject(models.Model):
     name = models.CharField(_("Name"), max_length=50)
-    abbreviation = models.CharField(_("Abbreviation"), max_length=4)
-    classes = models.ManyToManyField("classes.Class", verbose_name=_("Classes"))
+    abbreviation = models.CharField(_("Abbreviation"), max_length=4, null=True, blank=True)
+    code = models.CharField(_("Code"), max_length=3, unique=True)
+    level = models.CharField(
+        _("Level"),
+        max_length=1,
+        choices=LevelChoices.choices,
+    )
+    classes = models.ManyToManyField(Class, verbose_name=_("Classes"), related_name="subjects", blank=True)
+    is_compulsory = models.BooleanField(_("Is Compulsory"), default=False)
+    category = models.ForeignKey(
+        "Category",
+        verbose_name=_("Category"),
+        on_delete=models.CASCADE,
+        related_name="subjects",
+        null=True,
+    )
 
     class Meta:
         verbose_name = _("Subject")
@@ -16,8 +32,9 @@ class Subject(models.Model):
 
 
 class Paper(models.Model):
-    code = models.CharField(_("Paper Code"), max_length=10)
-    title = models.CharField(_("Title"), max_length=20, null=True)
+    subject = models.ForeignKey(Subject, verbose_name=_("Subject"), on_delete=models.CASCADE, related_name="papers")
+    number = models.CharField(_("Number"), max_length=4)
+    name = models.CharField(_("Name"), max_length=20, null=True)
 
     class Meta:
         verbose_name = _("Paper")
@@ -25,6 +42,22 @@ class Paper(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_paper_code(self):
+        return f"{self.subject.code}/{self.number}"
+
+
+class Category(models.Model):
+
+    name = models.CharField(_("Name"), max_length=20)
+
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+        return self.name
+
 
 
 
