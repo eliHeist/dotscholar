38cac05c030bb80dd52dfbe78b24d22de639ebd7
Elijah Triumph <eliyang256@outlook.com>
2025-05-23 09:55:43 +0300

    parent model

--- people/parents/models.py
+++ people/parents/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Parent(models.Model):
+
+    full_name = models.CharField(max_length=25, verbose_name="Full Name")
+    email = models.EmailField(_("Email"), max_length=254, null=True, blank=True)
+    phone = models.PhoneNumberField(_("Phone"), max_length=15)
+    phone_2 models.PhoneNumberField(_("Phone 2"), max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=255, verbose_name="Address")
+    students = models.ManyToManyField(Student, verbose_name=_("Students"))
+    
+    relation_role = models.CharField(
+        max_length=50,
+        choices=[
+            ("father", _("Father")),
+            ("mother", _("Mother")),
+            ("guardian", _("Guardian")),
+            ("brother", _("Brother")),
+            ("sister", _("Sister")),
+            ("uncle", _("Uncle")),
+            ("aunt", _("Aunt")),
+            ("grandfather", _("Grandfather")),
+            ("grandmother", _("Grandmother")),
+            ("other", _("Other")),
+        ],
+        default="mother",
+    )
+    
+
+    class Meta:
+        verbose_name = _("parent")
+        verbose_name_plural = _("parents")
+
+    def __str__(self):
+        return self.full_name
+
