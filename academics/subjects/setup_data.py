2d80e723ed88efc61601e7bdd6816f02c2a3d0dc
Elijah Triumph <eliyang256@outlook.com>
2025-05-23 11:34:38 +0300

    add O-Level Subjects

---
+++ academics/subjects/setup_data.py
@@ -0,0 +1,182 @@
+O_level_subjects = {
+    "456": {
+        "subject": "Mathematics",
+        "category": "Science",
+        "papers": ["1: Mathematics"]
+    },
+    "545": {
+        "subject": "Chemistry",
+        "category": "Science",
+        "papers": ["1: Chemistry Paper 1", "2: Chemistry Paper 2", "3: Chemistry Practical"]
+    },
+    "273": {
+        "subject": "Geography",
+        "category": "Art",
+        "papers": ["1: Geography"]
+    },
+    "553": {
+        "subject": "Biology",
+        "category": "Science",
+        "papers": ["1: Biology Paper 1", "2: Biology Paper 2", "3: Biology Practical"]
+    },
+    "535": {
+        "subject": "Physics",
+        "category": "Science",
+        "papers": ["1: Physics Paper 1", "2: Physics Paper 2", "3: Physics Practical"]
+    },
+    "612": {
+        "subject": "Art",
+        "category": "Art",
+        "papers": ["1: Art History & Studio Technology", "2: Art Making"]
+    },
+    "500": {
+        "subject": "General Science",
+        "category": "Science",
+        "papers": ["2: General Science Paper 2", "3: General Science Practical"]
+    },
+    "621": {
+        "subject": "Music",
+        "category": "Art",
+        "papers": ["1: Aural, Composition and Theory"]
+    },
+    "840": {
+        "subject": "ICT",
+        "category": "Science",
+        "papers": ["1: ICT Paper 1", "2: ICT Practical"]
+    },
+    "527": {
+        "subject": "Agriculture",
+        "category": "Science",
+        "papers": ["1: Agriculture Paper 1", "2: Agriculture Practical"]
+    },
+    "223": {
+        "subject": "Christian Religious Education",
+        "category": "Religion",
+        "papers": ["1: Christian Religious Education"]
+    },
+    "225": {
+        "subject": "Islamic Religious Education",
+        "category": "Religion",
+        "papers": ["1: Islamic Religious Education"]
+    },
+    "336": {
+        "subject": "Kiswahili",
+        "category": "Language",
+        "papers": ["1: Kiswahili Paper 1", "2: Kiswahili Paper 2"]
+    },
+    "112": {
+        "subject": "English",
+        "category": "Language",
+        "papers": ["1: English Language"]
+    },
+    "241": {
+        "subject": "History",
+        "category": "Art",
+        "papers": ["1: History & Political Education"]
+    },
+    "845": {
+        "subject": "Entrepreneurship",
+        "category": "Art",
+        "papers": ["1: Entrepreneurship"]
+    },
+    "208": {
+        "subject": "Literature",
+        "category": "Art",
+        "papers": ["1: Literature in English"]
+    },
+    "745": {
+        "subject": "Technology and Design",
+        "category": "Science",
+        "papers": ["1: Technology and Design Paper 1", "2: Technology and Design Paper 2"]
+    },
+    "397": {
+        "subject": "Ugandan Sign Language",
+        "category": "Language",
+        "papers": ["1: Ugandan Sign Language"]
+    },
+    "662": {
+        "subject": "Nutrition & Food Technology",
+        "category": "Science",
+        "papers": ["1: Nutrition & Food Technology"]
+    },
+    "301": {
+        "subject": "Latin",
+        "category": "Language",
+        "papers": ["1: Latin"]
+    },
+    "305": {
+        "subject": "Leb Acoli",
+        "category": "Language",
+        "papers": ["1: Leb Acoli Paper 1", "2: Leb Acoli Paper 2"]
+    },
+    "315": {
+        "subject": "Leblango",
+        "category": "Language",
+        "papers": ["1: Leblango Paper 1", "2: Leblango Paper 2"]
+    },
+    "325": {
+        "subject": "LugbaraTi",
+        "category": "Language",
+        "papers": ["1: LugbaraTi Paper 1", "2: LugbaraTi Paper 2"]
+    },
+    "335": {
+        "subject": "Luganda",
+        "category": "Language",
+        "papers": ["1: Luganda Paper 1", "2: Luganda Paper 2"]
+    },
+    "345": {
+        "subject": "Runyankore/Rukiga",
+        "category": "Language",
+        "papers": ["1: Runyankore/Rukiga Paper 1", "2: Runyankore/Rukiga Paper 2"]
+    },
+    "355": {
+        "subject": "Lusoga",
+        "category": "Language",
+        "papers": ["1: Lusoga Paper 1", "2: Lusoga Paper 2"]
+    },
+    "365": {
+        "subject": "Ateso",
+        "category": "Language",
+        "papers": ["1: Ateso Paper 1", "2: Ateso Paper 2"]
+    },
+    "375": {
+        "subject": "Dhopadhola",
+        "category": "Language",
+        "papers": ["1: Dhopadhola Paper 1", "2: Dhopadhola Paper 2"]
+    },
+    "385": {
+        "subject": "Runyoro/Rutooro",
+        "category": "Language",
+        "papers": ["1: Runyoro/Rutooro Paper 1", "2: Runyoro/Rutooro Paper 2"]
+    },
+    "395": {
+        "subject": "Lumasaaba",
+        "category": "Language",
+        "papers": ["1: Lumasaaba Paper 1", "2: Lumasaaba Paper 2"]
+    },
+    "337": {
+        "subject": "Arabic",
+        "category": "Language",
+        "papers": ["1: Arabic"]
+    },
+    "555": {
+        "subject": "Physical Education",
+        "category": "Science",
+        "papers": ["1: Physical Education"]
+    },
+    "309": {
+        "subject": "German",
+        "category": "Language",
+        "papers": ["1: German"]
+    },
+    "314": {
+        "subject": "French",
+        "category": "Language",
+        "papers": ["1: French"]
+    },
+    "396": {
+        "subject": "Chinese",
+        "category": "Language",
+        "papers": ["1: Chinese"]
+    }
+}
