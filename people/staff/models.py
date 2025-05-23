c508fd76ae02901b3be0d7565bcd346eea428c6f
Elijah Triumph <eliyang256@outlook.com>
2025-05-23 09:45:27 +0300

    primary role fiels

--- people/staff/models.py
+++ people/staff/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class Staff(models.Model):
+class RoleOptions(models.TextChoices):
+    TEACHER = 'Teacher', 'Teacher'
+    ADMINISTRATOR = 'Administrator', 'Administrator'
+    OTHER = 'Other', 'Other'
 
+class Staff(models.Model):
     first_name = models.CharField(_("First Name"), max_length=25)    
     middle_name = models.CharField(_("Middle Name"), max_length=25, null=True, blank=True)    
     last_name = models.CharField(_("Last Name"), max_length=25)
     gender = models.CharField(_("Gender"), max_length=5)
+    
+    primary_role = models.CharField(_("Primary Role"), max_length=50, choices=RoleOptions.choices)
 
     class Meta:
         verbose_name = _("Staff")
