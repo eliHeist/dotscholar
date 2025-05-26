8622549a0a4681976e38b566035d679731d8d480
Elijah Triumph <eliyang256@outlook.com>
2025-05-26 18:39:09 +0300

    add owner field to school

--- schools/schools/models.py
+++ schools/schools/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class School(models.Model):
 
     name = models.CharField(_("Name"), max_length=50)
     registration_number = models.CharField(_("Reg no."), max_length=50, unique=True)
     logo = models.FileField(_("Logo"), upload_to='school_logos', max_length=100, blank=True, null=True)
+    owner = models.OneToOneField(
+        User,
+        verbose_name=_("Owner"),
+        on_delete=models.SET_NULL,
+        related_name="school",
+        null=True,
+        blank=True
+    )
 
     class Meta:
         verbose_name = _("School")
