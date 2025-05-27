039bc5fed672187e358bb705e89dab09c7cdb948
Elijah Triumph <eliyang256@outlook.com>
2025-05-27 19:10:04 +0300

    Use the nice permissions form For tier permissions

--- subscriptions/tiers/admin.py
+++ subscriptions/tiers/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.models import Permission
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from .models import Tier
+
+class TierAdminForm(forms.ModelForm):
+    class Meta:
+        model = Tier
+        fields = "__all__"
+
+    permissions = forms.ModelMultipleChoiceField(
+        queryset=Permission.objects.all(),
+        widget=FilteredSelectMultiple("permissions", is_stacked=False),
+        required=False,
+    )
 
 @admin.register(Tier)
 class TierAdmin(admin.ModelAdmin):
+    form = TierAdminForm
     list_display = ("name", "price")
     search_fields = ("name",)
     list_filter = ("price",)
