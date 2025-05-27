f0177e63d9b970ae1383f0f720d4fcd017f70ad4
Elijah Triumph <eliyang256@outlook.com>
2025-05-28 01:37:01 +0300

    onboarding view and urls

--- misc/onboarding/views.py
+++ misc/onboarding/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class OnboardingView(View):
+    template_name = "onboarding/onboarding.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
