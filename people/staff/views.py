7be578de915ad57d73474451a8e99ed677e0b69c
Elijah Triumph <eliyang256@outlook.com>
2025-05-28 18:24:00 +0300

    staff list view

--- people/staff/views.py
+++ people/staff/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class StaffListView(View):
+    template_name = 'staff/staff_list.html'
+
+    def get(self, request, *args, **kwargs):
+        # Here you would typically fetch staff data from the database
+        # For demonstration purposes, we'll use a static list
+        staff_members = [
+            {'name': 'Alice Smith', 'position': 'Manager'},
+            {'name': 'Bob Johnson', 'position': 'Developer'},
+            {'name': 'Charlie Brown', 'position': 'Designer'},
+        ]
+        
+        context = {
+            'staff_members': staff_members
+        }
+        
+        return render(request, self.template_name, context)
+
