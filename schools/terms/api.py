95e70a5e5b9a5a7e9bf2b7a33e94f3d783035f16
Elijah Triumph <eliyang256@outlook.com>
2025-05-30 19:38:27 +0300

    terms api views

--- schools/terms/api.py
+++ schools/terms/api.py
@@ -4,6 +4,8 @@ from ninja import Router
 from ninja.errors import HttpError
 from ninja.security import django_auth
 
+from academics.classes.models import Class
+
 from .models import Term, TermFee
 from .schemas import TermSchemaIn, TermWithFeesSchemaOut
 
@@ -11,13 +13,25 @@ from .schemas import TermSchemaIn, TermWithFeesSchemaOut
 terms_router = Router(auth=django_auth)
 
 
-@terms_router.post("", response=TermWithFeesSchemaOut, url_name="add_term")
+@terms_router.post("", response=TermWithFeesSchemaOut, url_name="term-create")
 def create_term_with_fees(request, payload: TermSchemaIn):
+    print("Creating term with payload:")
+    print(payload)
     term_data = payload.dict(exclude={"term_fees"})
-    term = Term.objects.create(**term_data)
 
-    for fee_data in payload.term_fees:
-        TermFee.objects.create(term=term, **fee_data.dict())
+    with transaction.atomic():
+        classes = Class.objects.all()
+        term = Term.objects.create(**term_data)
+
+        for fee_data in payload.term_fees:
+            termFee = TermFee.objects.create(
+                term=term,
+                amount=fee_data.amount
+            )
+            fee_classes = [cls for cls in classes if cls.id in fee_data.classes]
+
+            termFee.classes.set(fee_classes)
+            termFee.save()
 
     return term
 
