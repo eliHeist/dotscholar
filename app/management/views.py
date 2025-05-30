f24ad81dccbfab2b3005962e4838b4ac61b9c011
Elijah Triumph <eliyang256@outlook.com>
2025-05-30 19:37:41 +0300

    get active term

--- app/management/views.py
+++ app/management/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 
+from schools.terms.models import Term
+
 # Create your views here.
 class ManagementOverview(View):
     template_name = 'management/overview.html'
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        # get active term for which this today is in 
+        active_term = Term.objects.filter(active=True).first()
+        context = {
+            'active_term': active_term,
+        }
         return render(request, self.template_name, context)
