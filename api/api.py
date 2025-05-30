425eaf2d74ddd47bf8ec4729afa8aae1d7a30b4f
Elijah Triumph <eliyang256@outlook.com>
2025-05-30 19:38:44 +0300

    register routers

--- api/api.py
+++ api/api.py
@@ -1,7 +1,12 @@
 from ninja import NinjaAPI
+
+from academics.classes.api import class_router
 from misc.onboarding.api import onboarding_router
+from schools.terms.api import terms_router
 
 
 api_router = NinjaAPI(urls_namespace="api")
 
 api_router.add_router("/onboarding/", onboarding_router)
+api_router.add_router("/classes/", class_router)
+api_router.add_router("/terms/", terms_router)
