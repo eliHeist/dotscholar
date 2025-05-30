0df2a78960e954619dcff86fc8f616a5ff1d4eab
Elijah Triumph <eliyang256@outlook.com>
2025-05-30 11:10:15 +0300

    term fees schemas

--- schools/terms/schemas.py
+++ schools/terms/schemas.py
@@ -1,9 +1,29 @@
-from ninja import ModelSchema
+from ninja import ModelSchema, Schema
+from ninja.orm import create_schema
 
-from .models import Term
+from .models import Term, TermFee
 
+class TermFeeSchemaIn(Schema):
+    classes: list[int]
+    amount: int
 
-class TermSchema(ModelSchema):
-    class Meta:
+class TermSchemaIn(Schema):
+    number: int
+    start_date: str
+    end_date: str
+    term_fees: list[TermFeeSchemaIn] = []
+
+TermSchemaOut = create_schema(Term, fields=["id", "number", "start_date", "end_date"])
+
+# TermWithFeesSchemaOut = create_schema(Term, fields=["id", "number", "start_date", "end_date"], custom_fields=[("term_fees", list[TermFeeSchemaIn], None)])
+
+class TermWithFeesSchemaOut(ModelSchema):
+    class Config:
         model = Term
         model_fields = "__all__"
+    
+    term_fees: list[TermFeeSchemaIn] = []
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.term_fees = [TermFeeSchemaIn(**fee) for fee in self.term_fees.all()]
