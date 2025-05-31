d5d0f50f9c649ad150f9c8a10a3eaee422125f50
Elijah Triumph <eliyang256@outlook.com>
2025-05-31 16:50:19 +0300

    class view

--- app/management/views.py
+++ app/management/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
+from academics.classes.models import Class
 from schools.terms.models import Term
 
 # Create your views here.
@@ -9,8 +10,28 @@ class ManagementOverview(View):
 
     def get(self, request, *args, **kwargs):
         # get active term for which this today is in 
-        current_term = Term.objects.filter(active=True).first()
+        user = request.user
+        school = user.get_school()
+        current_term = school.terms.filter(active=True).first()
         context = {
             'current_term': current_term,
+        }
+        return render(request, self.template_name, context)
+    
+class ManagementClassesView(View):
+    template_name = 'management/classes.html'
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        school = user.get_school()
+        classes = Class.objects.all()
+        streams = school.streams.all()
+
+        # annotate each stream to a class
+        for cls in classes:
+            cls.streams = streams.filter(current_class=cls)
+
+        context = {
+            'classes': classes,
         }
         return render(request, self.template_name, context)
