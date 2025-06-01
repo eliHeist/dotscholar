1e75219e479170e58debdcbc81bc047bfd878e0d
Elijah Triumph <eliyang256@outlook.com>
2025-06-01 23:04:56 +0300

    add rout for streams

--- api/api.py
+++ api/api.py
@@ -3,6 +3,7 @@ from ninja import NinjaAPI
 from academics.classes.api import class_router
 from misc.onboarding.api import onboarding_router
 from schools.terms.api import terms_router
+from schools.streams.api import streams_router
 
 
 api_router = NinjaAPI(urls_namespace="api")
@@ -10,3 +11,4 @@ api_router = NinjaAPI(urls_namespace="api")
 api_router.add_router("/onboarding/", onboarding_router)
 api_router.add_router("/classes/", class_router)
 api_router.add_router("/terms/", terms_router)
+api_router.add_router("/streams/", streams_router)
