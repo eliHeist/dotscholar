9b584fe2206adb706722842713281925cd103a2d
Elijah Triumph <eliyang256@outlook.com>
2025-06-02 14:27:30 +0300

    user schemas

--- accounts/schemas.py
+++ accounts/schemas.py
@@ -1,11 +1,34 @@
-from ninja import ModelSchema
+from ninja import ModelSchema, Schema
 
-from .models import User
+from .models import User, UserProfile
 
+
+
+class UserProfileSchema(ModelSchema):
+    class Meta:
+        model = UserProfile
+        fields = ["gender", "phone_1", "phone_2"]
 
 class UserSchema(ModelSchema):
-    class Config:
+    profile: UserProfileSchema = None
+    class Meta:
         model = User
-        model_fields = "__all__"
-        # exclude = ["password"]
-        model_exclude = ["password"]
+        fields = ["id", "email", "first_name", "last_name", "username", "is_active", "is_teacher"]
+    
+    
+    @staticmethod
+    def resolve_profile(obj):
+        return obj.profile or None
+
+
+
+
+class UserInSchema(Schema):
+    first_name: str
+    last_name: str
+    email: str
+    is_teacher: bool
+    gender: str
+    phone_1: str
+    phone_2: str
+    
