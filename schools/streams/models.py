99a8d41d75623475a35b3a403b1294a8e5a2c3c3
Elijah Triumph <eliyang256@outlook.com>
2025-06-02 23:52:44 +0300

    add class teacher

--- schools/streams/models.py
+++ schools/streams/models.py
@@ -3,13 +3,23 @@ from django.utils.translation import gettext_lazy as _
 
 from academics.classes.models import Class
 from schools.schools.models import School
+from people.teachers.models import Teacher
 
 # Create your models here.
 class Stream(models.Model):
 
     name = models.CharField(_("Name"), max_length=50, null=True, blank=True)
     school = models.ForeignKey(School, verbose_name=_("School"), on_delete=models.CASCADE, related_name="streams")
-    current_class = models.ForeignKey(Class, verbose_name=_("Class"), on_delete=models.CASCADE)
+    current_class = models.ForeignKey(Class, verbose_name=_("Class"), related_name="streams", on_delete=models.CASCADE)
+
+    current_class_teacher = models.ForeignKey(
+        Teacher, 
+        verbose_name=_("Current Class Teacher"), 
+        related_name="streams", 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True
+    )
 
     class Meta:
         verbose_name = _("Stream")
