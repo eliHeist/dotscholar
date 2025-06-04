207801b0145640ff46b0d5f7c8643277db30dca0
Elijah Triumph <eliyang256@outlook.com>
2025-06-05 02:28:25 +0300

    admin reg

--- schools/streams/admin.py
+++ schools/streams/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Stream
 
 # Register your models here.
+@admin.register(Stream)
+class StreamAdmin(admin.ModelAdmin):
+    list_display = ["name", "current_class", "class_teacher"]
