662d8c2d186b5304b613cb9686ecd0c01f3a67bc
Elijah Triumph <eliyang256@outlook.com>
2025-06-05 02:25:27 +0300

    add alpine ajax

--- dotscholar/assets/static/src/main.ts
+++ dotscholar/assets/static/src/main.ts
@@ -1,12 +1,13 @@
 import './style.css'
 // import 'lucide-static/font/lucide.css'
 import Alpine from "alpinejs";
+import ajax from '@imacrayon/alpine-ajax'
 import mask from "@alpinejs/mask"
 import collapse from "@alpinejs/collapse"
-import 'htmx.org';
 
 
 Alpine.plugin(mask)
 Alpine.plugin(collapse)
+Alpine.plugin(ajax)
 
 Alpine.start()
