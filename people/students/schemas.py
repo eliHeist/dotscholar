7d229dd6ab20c6136ba0057f3ae9d515b5343bc8
Elijah Triumph <eliyang256@outlook.com>
2025-06-05 15:47:39 +0300

    new student + enrollment schema

--- people/students/schemas.py
+++ people/students/schemas.py
@@ -1,4 +1,6 @@
-from ninja import ModelSchema
+from ninja import ModelSchema, Schema
+
+from schools.enrollment.schemas import EnrollmentSchema
 
 from .models import Student
 
@@ -7,3 +9,16 @@ class StudentSchema(ModelSchema):
     class Meta:
         model = Student
         model_fields = "__all__"
+
+
+
+class NewStudentEnrollmentSchema(Schema):
+    first_name:str
+    last_name:str
+    middle_name:str
+    gender:str
+    payment_code:str=None
+    
+    term:int
+    class_for:int
+    stream_for:int
