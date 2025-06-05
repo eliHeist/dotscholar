03b71f190f5b4ab0a89975d5bedbd764f32da13d
Elijah Triumph <eliyang256@outlook.com>
2025-06-05 15:25:18 +0300

    api view for subject management

--- academics/subjects/api.py
+++ academics/subjects/api.py
@@ -2,11 +2,12 @@ from typing import List
 
 from django.db import transaction
 
+from django.shortcuts import get_object_or_404
 from ninja import Router
 from ninja.security import django_auth
 
-from .models import SchoolPapersGroup, Paper
-from .schemas import PaperSchema, PaperRegistrationInSchema
+from .models import SchoolPapersGroup, Paper, SchoolPaperAssignment, Subject
+from .schemas import PaperIdSchema, PaperSchema, PaperRegistrationInSchema, SchoolPaperAssignmentSchema, CreateSchoolPapersGroupSchema, SubjectPapersSchema
 
 subjects_router = Router(auth=django_auth)
 
@@ -27,3 +28,39 @@ def register_papers(request, payload: PaperRegistrationInSchema):
         paper_group.save()
     
     return papers
+
+
+@subjects_router.post("/manage_school_papers/", url_name="manage_school_papers")
+def create_school_papers_group(request, data: List[SchoolPaperAssignmentSchema]):
+    school = request.user.get_school()
+    school_paper_group = school.paper_group
+
+    # Remove existing paper assignments that are not in the incoming data
+    existing_paper_ids = [assignment.paper_id for assignment in data]
+
+    school_paper_group.paper_assignments.exclude(paper_id__in=existing_paper_ids).delete()
+
+    # Add or update paper assignments from the incoming data
+    for assignment in data:
+        paper = get_object_or_404(Paper, id=assignment.paper_id)
+        school_paper_assignment, created = SchoolPaperAssignment.objects.get_or_create(
+            school_papers_group=school_paper_group,
+            paper=paper,
+            defaults={'is_compulsory': assignment.is_compulsory}
+        )
+        if not created:
+            school_paper_assignment.is_compulsory = assignment.is_compulsory
+            school_paper_assignment.save()
+
+    return {"message": "School papers group updated successfully"}
+
+@subjects_router.get("school_papers/", response=List[SchoolPaperAssignmentSchema], url_name="get_school_papers")
+def get_school_papers_group(request):
+    school = request.user.get_school()
+    return school.paper_group.paper_assignments.all()
+
+@subjects_router.get("all_subjects/", response=List[SubjectPapersSchema], url_name="all_subjects")
+def get_all_subjects(request):
+    school = request.user.get_school()
+    return Subject.objects.get_available_for_school(school).order_by('-name')
+
