6f5604c23f01907033f473e090bb87f74060d436
Elijah Triumph <eliyang256@outlook.com>
2025-06-05 15:24:56 +0300

    new paper registration schemas

--- academics/subjects/schemas.py
+++ academics/subjects/schemas.py
@@ -1,6 +1,6 @@
 # academics/subjects/schemas.py
 from ninja import ModelSchema, Schema
-from .models import Subject, Paper
+from .models import SchoolPaperAssignment, Subject, Paper
 
 
 class SubjectSchema(ModelSchema):
@@ -13,9 +13,39 @@ class PaperSchema(ModelSchema):
     class Meta:
         model = Paper
         fields = "__all__"
+
+class PaperIdSchema(ModelSchema):
+    class Meta:
+        model = Paper
+        fields = ["id"]
+
+class SchoolPaperAssignmentSchema(ModelSchema):
+    class Meta:
+        model = SchoolPaperAssignment
+        fields = ["paper", "is_compulsory"]
         
 
 class PaperRegistrationInSchema(Schema):
     level: str
     papers: list[int]
 
+
+
+class SchoolPaperAssignmentSchema(Schema):
+    paper_id: int
+    is_compulsory: bool
+
+class CreateSchoolPapersGroupSchema(Schema):
+    school_id: int
+    paper_assignments: list[SchoolPaperAssignmentSchema]
+    
+
+class SubjectPapersSchema(ModelSchema):
+    papers: list[PaperSchema] = []
+    class Meta:
+        model = Subject
+        fields = ["id", "name", "code", "level", "classes", "is_base"]
+    
+    @staticmethod
+    def resolve_papers(obj):
+        return obj.papers.all()
