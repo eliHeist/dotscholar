1ccf3c696a04dde6625df33f457d1c85c927d0a9
Elijah Triumph <eliyang256@outlook.com>
2025-06-07 01:00:19 +0300

    class full schema

--- academics/classes/schemas.py
+++ academics/classes/schemas.py
@@ -1,4 +1,7 @@
+from typing import List
 from ninja import ModelSchema
+
+from schools.streams.schemas import StreamSchema
 
 from .models import Class
 
@@ -7,3 +10,15 @@ class ClassSchema(ModelSchema):
     class Meta:
         model = Class
         fields = "__all__"
+
+
+class ClassFullSchema(ModelSchema):
+    school_streams: List[StreamSchema] = None
+    class Meta:
+        model = Class
+        fields = "__all__"
+        
+    @staticmethod
+    def resolve_streams(obj:Class):
+        return obj.school_streams
+
