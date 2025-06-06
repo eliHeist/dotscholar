028dc0878be4d504bc8ec46cff23421987615e32
Elijah Triumph <eliyang256@outlook.com>
2025-06-07 01:08:55 +0300

    register view

--- app/people/urls.py
+++ app/people/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.PeopleOverviewView.as_view(), name="overview"),
     path("users/", views.PeopleUserView.as_view(), name="users"),
     path("students/", views.StudentsManagementView.as_view(), name="students"),
+    path("students/new-enrollments/", views.StudentsNewEnrollmentsView.as_view(), name="new-students-enrollment"),
 ]
