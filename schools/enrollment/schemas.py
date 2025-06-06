abb2c034c7f2f47b2e450dc1699cd2aa99723e9c
Elijah Triumph <eliyang256@outlook.com>
2025-06-07 01:01:26 +0300

    enrollment (admin and schema)

--- schools/enrollment/schemas.py
+++ schools/enrollment/schemas.py
@@ -1,4 +1,6 @@
 from ninja import ModelSchema, Schema
+
+from people.students.schemas import StudentSchema
 from .models import Enrollment
 
 
@@ -6,4 +8,23 @@ class EnrollmentSchema(ModelSchema):
     class Meta:
         model = Enrollment
         fields = "__all__"
+
+
+class EnrollmentDisplaySchema(ModelSchema):
+    student: StudentSchema
+    stream_name: str
+    term_name: str
+    class Meta:
+        model = Enrollment
+        fields = ["id", "student", "date_enrolled"]
+    
+    @staticmethod
+    def resolve_stream_name(self):
+        return self.stream_for.get_full_name()
+    
+    @staticmethod
+    def resolve_term_name(self):
+        return self.term.get_display_name
+    
+
         
