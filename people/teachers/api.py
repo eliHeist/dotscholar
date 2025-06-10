bb6a03140416d475cc070c8a44778e0ba8f8417e
Elijah Triumph <eliyang256@outlook.com>
2025-06-10 22:54:31 +0300

    teaching asignment apis

--- people/teachers/api.py
+++ people/teachers/api.py
@@ -1,7 +1,7 @@
 from ninja import Router
 from ninja.security import django_auth
 
-from .schemas import TeacherSchema
+from .schemas import TeacherSchema, TeachingAssignmentInSchema
 from .models import Teacher
 
 teacher_router = Router(auth=django_auth)
@@ -14,3 +14,12 @@ def list_teachers(request):
     """
     school = request.user.get_school()
     return Teacher.objects.get_by_school(school)
+
+
+# @teacher_router.get("", response=list[TeachingAssignmentInSchema], url_name="get_assignments")
+# def get_assignments(request):
+#     """
+#     List all teachers.
+#     """
+#     school = request.user.get_school()
+#     return Teacher.objects.get_by_school(school)
--- people/teachers/schemas.py
+++ people/teachers/schemas.py
@@ -1,9 +1,20 @@
+from typing import List
 from ninja import ModelSchema, Schema
 
-from .models import Teacher
+from .models import Teacher, TeachingAssignment
 
 
 class TeacherSchema(ModelSchema):
     class Meta:
         model = Teacher
         fields = ["id", "first_name", "last_name", "email", "username", "is_active"]
+
+
+class TeachingAssignmentInSchema(Schema):
+    id: int
+    teacher: int
+    subject: int
+    papers: list[int] = None
+    class_group: int
+    streams: list[int] = None
+    
