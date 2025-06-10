cf6b889ae0d550cc50ccc18f2f6782c2aa450ac5
Elijah Triumph <eliyang256@outlook.com>
2025-06-10 18:31:08 +0300

    student mark model

--- academics/marks/models.py
+++ academics/marks/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from academics.assessments.models import Assessment
+from people.students.models import Student
+from people.teachers.models import Teacher
+
+
+class Mark(models.Model):
+
+    assessment = models.ForeignKey(Assessment, verbose_name=_("Assessment"), on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, verbose_name=_("Student"), on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField(_("Score"))
+    remarks = models.TextField(_("Remarks"), null=True, blank=True)
+    teacher = models.ForeignKey(
+        Teacher,
+        verbose_name=_("Teacher"),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = _("Mark")
+        verbose_name_plural = _("Marks")
+        unique_together = ("assessment", "student")
+
+    def __str__(self):
+        return self.name
+
+
