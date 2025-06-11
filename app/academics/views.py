49e426a1debeaa3ea6116912dd2f58ba2044d97e
Elijah Triumph <eliyang256@outlook.com>
2025-06-11 23:28:01 +0300

    class view showing teachers allowed class

--- app/academics/views.py
+++ app/academics/views.py
@@ -2,21 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+from academics.classes.models import Class
+from people.teachers.models import Teacher
+
 # Create your views here.
-class ClassesAcademicsView(LoginRequiredMixin, View):
-    template_name = "academics/classes.html"
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        school = user.get_school()
-        classes = school.get_classes()
-        
-        context = {
-            "classes": classes
-        }
-        return render(request, self.template_name, context)
-
-
 class ClassSubjectsView(View):
     def get(self, request, class_pk, *args, **kwargs):
         school = request.user.get_school()
@@ -27,3 +16,38 @@ class ClassSubjectsView(View):
             "class": class_,
         }
         return render(request, template_name, context)
+
+
+class ClassesAcademicsView(LoginRequiredMixin, View):
+    template_name = "academics/classes.html"
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        school = user.get_school()
+        classes = school.get_bare_classes()
+        
+        teacher_object = Teacher.objects.get(pk=user.pk)
+        
+        assignments = teacher_object.teaching_assignments.all()
+
+        streams = []
+        for assignment in assignments:
+            stream = assignment.stream
+            streams.append(stream)
+        
+        for cls in classes:
+            cls.stream_assignments = set() # to hold the streams that are matched
+            for stream in streams:
+                if stream.current_class == cls:
+                    cls.stream_assignments.add(stream)
+                
+                # indicate if the stream belongs to the logged in user
+                stream.mine = (stream.class_teacher == request.user)
+        
+        context = {
+            "classes": classes
+        }
+        return render(request, self.template_name, context)
+
+
+
